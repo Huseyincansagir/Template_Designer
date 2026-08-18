d66db5327f28a25aefdafbe35f016ed3adeaa59d
HÜSEYİN CAN SAĞIR <93519611+Huseyincansagir@users.noreply.github.com>
2026-08-18 03:05:50 +0300

    feat: add Tauri application entrypoint

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#[tauri::command]
+fn app_version() -> &'static str {
+    env!("CARGO_PKG_VERSION")
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![app_version])
+        .run(tauri::generate_context!())
+        .expect("error while running Template Designer");
+}
